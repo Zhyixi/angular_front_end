eb0d5804618a8685054f4dfcefd4e574e17b2ea2
張翊翔 Sean Chang <zhyixi82105@gmail.com>
2024-02-08 00:43:24 +0800

    tmp

---
+++ accouting/src/main.ts
@@ -0,0 +1,8 @@
+// 應用程式的主模組定義
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
